188077d69c296704ae00de0e095839bbcb434d82
Ana-brinon <61994893+Ana-brinon@users.noreply.github.com>
2021-06-24 15:06:11 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+
+function myFuntion (){
+    var element = document.getElementById("burbujas");
+    element.classList.toggle("colorred")   
+    }
+    
+function myFuntion1 (){
+    var element = document.getElementById("burbujas1");
+    element.classList.toggle("colorred")
+    }
+ function myFuntion2 (){
+        var element = document.getElementById("burbujas2");
+        element.classList.toggle("colorred")
+        }
+function myFuntion3 (){
+            var element = document.getElementById("burbujas3");
+            element.classList.toggle("colorred")
+            }
+function myFuntion4 (){
+                var element = document.getElementById("burbujas4");
+                element.classList.toggle("colorred")
+                }
+ function myFuntion5 (){
+                    var element = document.getElementById("burbujas5");
+                    element.classList.toggle("colorred")
+ }
+ function myFuntion6 (){
+    var element = document.getElementById("burbujas6");
+    element.classList.toggle("colorred")
+}
+function myFuntion7 (){
+    var element = document.getElementById("burbujas7");
+    element.classList.toggle("colorred")
+}
+function myFuntion8 (){
+    var element = document.getElementById("burbujas8");
+    element.classList.toggle("colorred")
+}
+function myFuntion9 (){
+    var element = document.getElementById("burbujas9");
+    element.classList.toggle("colorred")
+}
+function myFuntion10 (){
+    var element = document.getElementById("burbujas10");
+    element.classList.toggle("colorred")
+}
+function myFuntion11 (){
+    var element = document.getElementById("burbujas11");
+    element.classList.toggle("colorred")
+}
+function myFuntion12 (){
+    var element = document.getElementById("burbujas12");
+    element.classList.toggle("colorred")
+}
